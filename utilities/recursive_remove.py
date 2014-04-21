081a97d6b64f322491f0ab734f19d979132c7764
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-04-21 10:45:55 +0200

    Implement "PEP8" naming convention.

--- utilities/recursive_remove.py
+++ utilities/recursive_remove.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-**recursiveRemove.py**
+**recursive_remove.py**
 
 **Platform:**
 	Windows, Linux, Mac Os X.
@@ -22,6 +22,7 @@ from __future__ import unicode_literals
 #**********************************************************************************************************************
 #***	External imports.
 #**********************************************************************************************************************
+import argparse
 import os
 import sys
 
@@ -35,28 +36,29 @@ __maintainer__ = "Thomas Mansencal"
 __email__ = "thomas.mansencal@gmail.com"
 __status__ = "Production"
 
-__all__ = ["recursiveRemove", "remove"]
+__all__ = ["recursive_remove", "remove", "get_command_line_arguments" , "main"]
 
 #**********************************************************************************************************************
 #***	Module classes and definitions.
 #**********************************************************************************************************************
-def recursiveRemove(rootDirectory, pattern):
+def recursive_remove(root_directory, pattern):
 	"""
 	Recursively deletes the matching items.
 
-	:param rootDirectory: Directory to recurse.
-	:type rootDirectory: unicode
+	:param root_directory: Directory to recurse.
+	:type root_directory: unicode
 	:param pattern: Pattern to match.
 	:type pattern: unicode
 	"""
 
-	if not os.path.exists(rootDirectory):
+	if not os.path.exists(root_directory):
 		return
-	for root, dirs, files in os.walk(rootDirectory):
+
+	for root, dirs, files in os.walk(root_directory, followlinks=True):
 		for item in files:
-			itemPath = os.path.join(root, item).replace("\\", "/")
+			item_path = os.path.join(root, item).replace("\\", "/")
 			if pattern in item:
-				remove(itemPath)
+				remove(item_path)
 
 def remove(item):
 	"""
@@ -71,5 +73,50 @@ def remove(item):
 	except:
 		print("{0} | '{1}' file removing failed!".format(remove.__name__, item))
 
+def get_command_line_arguments():
+	"""
+	Retrieves command line arguments.
+
+	:return: Namespace.
+	:rtype: Namespace
+	"""
+
+	parser = argparse.ArgumentParser(add_help=False)
+
+	parser.add_argument("-h",
+						"--help",
+						action="help",
+						help="'Displays this help message and exit.'")
+
+	parser.add_argument("-i",
+						"--input",
+						type=unicode,
+						dest="input",
+						help="'Input directory to recurse.'")
+
+	parser.add_argument("-p",
+						"--pattern",
+						type=unicode,
+						dest="pattern",
+						help="'Pattern to match.'")
+
+	if len(sys.argv) == 1:
+		parser.print_help()
+		sys.exit(1)
+
+	return parser.parse_args()
+
+def main():
+	"""
+	Starts the Application.
+
+	:return: Definition success.
+	:rtype: bool
+	"""
+
+	args = get_command_line_arguments()
+	return 0 if recursive_remove(args.input, args.pattern) else 1
+
 if __name__ == "__main__":
-	recursiveRemove(sys.argv[1], sys.argv[2])
+	main()
+
